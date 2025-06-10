b7d1b32a54498bed075a321bbe93ea94c1e51571
akshata <akshatak2810@gmail.com>
2025-06-10 13:16:20 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ const App=() => {
         <div>
             <BrowserRouter>
             <Routes>
-                <Route path="/home" element={<HomePage/>} />
+                <Route path="/" element={<HomePage/>} />
                 <Route path="/about" element={<AboutPage/>} />
                 <Route path="/courses" element={<CoursesPage/>} />
                 <Route path="/contact" element={<ContactPage/>} />
