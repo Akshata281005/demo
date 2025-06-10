2906f26f33899ddc1ac8571add71dac316c9c62c
akshata <akshatak2810@gmail.com>
2025-06-10 10:26:17 +0530

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="foot">
+                <p>
+                    "Copyright"
+                    "2025"
+                    " Vivekanand College. All rights reserved."
+                </p>
+            </footer>  
+        </div>
+    )
+}
+
+export default Footer
